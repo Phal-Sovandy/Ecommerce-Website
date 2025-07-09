d65c3ea33d254b86b5cfccd8ed4434bfccb73541
PinRathana <pinrathana1@gmail.com>
2025-07-10 05:10:08 +0700

    Update reviewQuery.js
    query reviews

--- Ecommerce-Website/backEnd/repositories/reviewQuery.js
+++ Ecommerce-Website/backEnd/repositories/reviewQuery.js
@@ -1,12 +1,72 @@
 import models from "../models/index.js";
 
-// Query all review by customer_id
-// TODO
-// Query all review by ASIN (product_id)
-// TODO
-// Add customer review
-// TODO
-// Delete customer review
-// TODO
-// Edit customer review
-// TODO
+export async function queryAllReviews() {
+  try {
+    return await models.CustomerReview.findAll({
+      include: [
+        { model: models.Product, as: "product" },
+        { model: models.Customer, as: "customer" },
+      ],
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Query all reviews by customer_id
+export async function queryReviewsByCustomerId(customer_id) {
+  try {
+    return await models.CustomerReview.findAll({
+      where: { customer_id },
+      include: [{ model: models.Product, as: "product" }],
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Query all reviews by ASIN (product id)
+export async function queryReviewsByAsin(asin) {
+  try {
+    return await models.CustomerReview.findAll({
+      where: { asin },
+      include: [{ model: models.Customer, as: "customer" }],
+    });
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Add a new customer review
+export async function addCustomerReview(reviewData) {
+  try {
+    const newReview = await models.CustomerReview.create(reviewData);
+    return newReview;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Delete a customer review by review_id
+export async function deleteCustomerReview(review_id) {
+  try {
+    const deleted = await models.CustomerReview.destroy({
+      where: { review_id },
+    });
+    return deleted; // returns number of rows deleted (0 or 1)
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Edit/update a customer review by review_id
+export async function updateCustomerReview(review_id, updatedData) {
+  try {
+    const [updatedRowsCount] = await models.CustomerReview.update(updatedData, {
+      where: { review_id },
+    });
+    return updatedRowsCount; // number of updated rows (0 or 1)
+  } catch (error) {
+    throw error;
+  }
+}
